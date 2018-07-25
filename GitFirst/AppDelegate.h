1519941cffee11fb6913bab59ca24ab8769ecbd9
tianfengLu <13244303311@163.com>
2018-07-25 09:22:25 +0800

    Initial Commit

---
+++ GitFirst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitFirst
+//
+//  Created by Taffy on 2018/7/25.
+//  Copyright © 2018年 Taffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
